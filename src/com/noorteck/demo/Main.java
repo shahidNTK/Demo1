f00874980e4f9fb98b49230378013a7b0922816f
Shahid Ahmadi <shahid.ahmadi@noorteck.com>
2023-01-10 20:25:27 -0500

    Created main class to run java application

---
+++ src/com/noorteck/demo/Main.java
@@ -0,0 +1,9 @@
+package com.noorteck.demo;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("This is Java class available in my local workspace ");
+	}
+
+}
