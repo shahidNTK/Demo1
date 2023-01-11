461435f2b8ea5fae413dbf70cfddadb9c60addcf
Shahid Ahmadi <shahid.ahmadi@noorteck.com>
2023-01-10 21:06:19 -0500

    4th commit

--- src/com/noorteck/demo/Main.java
+++ src/com/noorteck/demo/Main.java
@@ -4,7 +4,6 @@ public class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("This is Java class available in my local workspace ");
-		System.out.println("Second message");
 	}
 
 }
